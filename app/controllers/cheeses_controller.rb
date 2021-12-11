99daaab0e77b2c37210c6700a6de307a49a131f3
Craig <craigjope@gmail.com>
2021-12-11 02:16:59 -0700

    commit tree 27f4ae0ed0a17ec4fcac18ec6730dae2b8c7a923

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,17 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
+    if cheese
     render json: cheese
+    else
+      render json: {error: "Your search was no gouda"}
+    end
   end
 
 end
